1f6ec5e95972e21e6c469d3a92111007b528c12c
rrrafalsz <rrrafalsz@gmail.com>
2017-11-25 16:58:16 +0000

    create tests stub

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed, async } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
+import {RouterTestingModule} from '@angular/router/testing';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -8,6 +9,9 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      imports: [
+        RouterTestingModule
+      ]
     }).compileComponents();
   }));
 
@@ -17,16 +21,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app'`, async(() => {
+  it(`should have as title 'cashtrack'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    expect(app.title).toEqual('cashtrack');
   }));
 });
--- src/app/cash-table/cash-table.component.spec.ts
+++ src/app/cash-table/cash-table.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CashTableComponent } from './cash-table.component';
+import {ItemService} from '../item.service';
 
 describe('CashTableComponent', () => {
   let component: CashTableComponent;
@@ -8,7 +9,8 @@ describe('CashTableComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CashTableComponent ]
+      declarations: [ CashTableComponent ],
+      providers: [ ItemService ]
     })
     .compileComponents();
   }));
@@ -22,4 +24,20 @@ describe('CashTableComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should display a table', () => {});
+
+  it('the table should fill the screen on desktop', () => {});
+
+  it('the table should scroll on mobile', () => {});
+
+  it('the table items should be editable in-place', () => {});
+
+  it('should fetch items', () => {});
+
+  it('should use the ItemService', () => {});
+
+  it('should display fetched items in the table', () => {});
+
+  it('each item should be displayed in a single row', () => {});
 });
