867ebc5e1e41966eac0b68bfe59303a8686ce0f6
rrrafalsz <rrrafalsz@gmail.com>
2018-02-23 14:56:41 -0300

    add missing AppComponent tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,14 @@
-import {async, TestBed} from '@angular/core/testing';
+import {async, ComponentFixture, inject, TestBed} from '@angular/core/testing';
 
 import {AppComponent} from './app.component';
 import {RouterTestingModule} from '@angular/router/testing';
 import {ItemService} from '../api/item.service';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  // let service: ItemService;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
@@ -19,15 +23,33 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+  });
+
+  /*beforeEach(inject([ItemService], (itemService) => {
+    service = itemService;
+  }));*/
+
   it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
 
   it(`should have as title 'cashtrack'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('cashtrack');
   }));
+
+  it('should have an addItem method', () => {
+    const method = component.addItem;
+    expect(method).toBeDefined();
+  });
+
+  // TODO
+  /*
+  it('the addItem method should invoke the item service', () => {
+
+  });*/
 });
